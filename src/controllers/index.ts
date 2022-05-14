e82336adaa9f81b82ecff06ee00ddfc3ad551155
valuemount <projects.valuemount@gmail.com>
2022-05-15 04:52:27 +0530

    initial commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './hello.controller';
+export * from './ping.controller';
+
+export * from './todo.controller';
+export * from './user.controller';
---
+++ src/models/todo.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  desc: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  isCompleted: boolean;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
